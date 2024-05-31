39fc608a5483ca5308311030694855839e19e050
Mki Pokryvailo <mpokryva@gmail.com>
2024-05-31 16:02:57 -0400

    logos fix maybe

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 import { defineConfig } from 'astro/config'
-import compress from "astro-compress";
+import compress from 'astro-compress'
 
 import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://mpokryva.github.io',
-    devToolbar: {
-        enabled: false
-    },
-    integrations: [tailwind(), compress()],
-});
+  site: 'https://mpokryva.github.io',
+  devToolbar: {
+    enabled: false,
+  },
+  integrations: [tailwind(), compress()],
+})
