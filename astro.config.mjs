0803d082b50068106a99cf722f115d28d8901f4e
Mki Pokryvailo <mpokryva@gmail.com>
2024-05-31 16:35:50 -0400

    configure itsmiki.com domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://mpokryva.github.io',
+  site: 'https://itsmiki.com',
   devToolbar: {
     enabled: false,
   },
