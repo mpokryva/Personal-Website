674fe21d0c273db2ab584e700e5f6b422764d1b3
Mki Pokryvailo <mpokryva@gmail.com>
2025-08-03 17:00:15 -0700

    convert to next

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -2,13 +2,4 @@
 export default {
   semi: false,
   singleQuote: true,
-  plugins: ['prettier-plugin-astro'],
-  overrides: [
-    {
-      files: '*.astro',
-      options: {
-        parser: 'astro',
-      },
-    },
-  ],
 }
